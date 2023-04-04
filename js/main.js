855b3e5107efe48526ff08761cbd1a52b3371123
SingupalliKartik <skartik1706@gmail.com>
2023-04-05 00:45:45 +0530

    1st

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+      const skills_wrap = document.querySelector(".about-skills"),
+      skills_bar = document.querySelectorAll(".progress-line");
+      window.addEventListener("scroll", () => {
+          skillsEffect();
+      })
+
+      function checkScroll(el)
+      {
+
+          let rect = el.getBoundingClientRect();
+     
+          if(window.innerHeight >= rect.top + el.offsetHeight) return true;
+          return false;
+      }
+      function skillsEffect()
+      {
+          if(!checkScroll(skills_wrap)) return;
+          skills_bar.forEach((skill) => (skill.style.width = skill.dataset.progress));
+      }
+  
